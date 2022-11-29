50558cfaae80595adbdb5b544603df7a7ac6849d
mka-Mikhail <mixailrojnow@yandex.ru>
2022-11-29 22:32:23 +0300

    Initial commit

---
+++ src/main/java/com/mka/springmvcspringboot/SpringMvcSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.mka.springmvcspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mka/springmvcspringboot/SpringMvcSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mka.springmvcspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
