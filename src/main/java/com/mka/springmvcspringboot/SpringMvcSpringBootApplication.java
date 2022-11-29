78a4e5775ac853a8078783f77e2f124c594f2b04
mka-Mikhail <mixailrojnow@yandex.ru>
2022-11-29 22:32:30 +0300

    init

--- src/main/java/com/mka/springmvcspringboot/SpringMvcSpringBootApplication.java
+++ src/main/java/com/mka/springmvcspringboot/SpringMvcSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringMvcSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMvcSpringBootApplication.class, args);
+    }
 
 }
